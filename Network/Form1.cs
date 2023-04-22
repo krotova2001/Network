890700a76a88a1256b986a56e44f3f348ac85abf
мк <@WKS-220>
2023-04-22 20:52:38 +0700

    Добавьте файлы проекта.

---
+++ Network/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Network
+{
+    public partial class Form1 : Form
+    {
+        Socket socket;
+        string data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //connect
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress IPAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(IPAddress, 777);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(endPoint);
+                if (socket.Connected)
+                {
+                    byte[] buffer = new byte[4096];
+                    int lenth = 0;
+                    lenth = socket.Receive(buffer);
+                    data = Encoding.Default.GetString(buffer, 0, lenth);
+                }
+
+                socket.Shutdown(SocketShutdown.Both);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { socket.Close(); }
+            richTextBox1.Text += data;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String s = richTextBox1.Text;
+            socket.Send(Encoding.Default.GetBytes(s));
+        }
+    }
+}
---
+++ Server/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress IPAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(IPAddress, 777);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Task task = Task.Run(() => { Start_server(socket, endPoint); });
+            
+        }
+
+        private void Start_server (Socket s, EndPoint e)
+        {
+            try
+            {
+                s.Bind(e);
+                s.Listen(10);
+                while (true)
+                {
+                   Socket s_client = s.Accept();
+                   Task task = Task.Run(() => { Send_otvet(s_client); });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { s.Close(); }
+        }
+
+        private void Send_otvet(Socket s_client)
+        {
+            byte[] buf = new byte[1024];
+            int l = s_client.Receive(buf);
+            string mess = Encoding.Default.GetString(buf, 0, l);
+            s_client.Send(Encoding.Default.GetBytes(DateTime.Now.ToString() + " " + mess));
+            s_client.Shutdown(SocketShutdown.Both);
+            s_client.Close();
+        }
+    }
+}
